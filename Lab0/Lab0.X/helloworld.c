ad481efeedcec01d45939dad78be431493b0b6a6
Nadia Shabbar <nshabbar@ucsc.edu>
2024-01-09 16:37:38 -0800

    Buffered the AD reading.

--- Lab0/Lab0.X/helloworld.c
+++ Lab0/Lab0.X/helloworld.c
@@ -18,10 +18,14 @@ int main(int argc, char** argv) {
     AD_AddPins(AD_A0);
     printf("Hello world!\n");
     BOARD_End();
-    
-    while (1){
-        unsigned int pot_val = AD_ReadADPin(AD_A0);
-        printf("%d\n", pot_val);
+    unsigned int pot_val = 0;
+
+    while (1) {
+        unsigned int new_pot_val = AD_ReadADPin(AD_A0);
+        if (pot_val != new_pot_val) {
+            pot_val = new_pot_val;
+            printf("%d\n", pot_val);
+        }
     }
 }
 
