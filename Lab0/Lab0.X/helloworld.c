06a118fbe678ba4d3b242926031ac2b53489e6fd
Nadia Shabbar <nshabbar@ucsc.edu>
2024-01-10 18:11:47 -0800

    Adding finished progress.

--- Lab0/Lab0.X/helloworld.c
+++ Lab0/Lab0.X/helloworld.c
@@ -14,50 +14,101 @@
 #include "ToneGeneration.h"
 #include "Buttons.h"
 #define sample_size 300
+uint8_t button_avg_flag = FALSE;
+unsigned int frequency = DEFAULT_TONE;
 
 int main(int argc, char** argv) {
     BOARD_Init();
     AD_Init();
     ToneGeneration_Init();
     AD_AddPins(AD_A0);
-    printf("Hello world!\n");
+    printf("Hello world!\n"); // Hello world
     BOARD_End();
-    unsigned int pot_val = 0;
+    //    unsigned int pot_val = 0;
     unsigned int move_average_readings[sample_size] = {0}; // array to store samples of frequency
     unsigned int index = 0; // current index
     unsigned int sum = 0; // sum of all readings
+    unsigned int new_average = 0;
+    unsigned int average =0;
 
 
     while (1) {
-        unsigned int new_pot_val = AD_ReadADPin(AD_A0);
-        if (pot_val != new_pot_val) {
-            pot_val = new_pot_val;
-            printf("%d\n", pot_val);
-        }
-        sum = sum - move_average_readings[index] + new_pot_val; // subtract oldest reading and add new one
-        move_average_readings[index] = new_pot_val; // store the new reading
-
-        unsigned int average = sum / sample_size; // calculate the moving average
-
-        index = (index + 1) % sample_size; // this will increment and wrap index
-
         uint8_t bEvent = ButtonsCheckEvents();
-        if (bEvent & BUTTON_EVENT_1DOWN) {
-            ToneGeneration_SetFrequency(average + TONE_196);
-            ToneGeneration_ToneOn();
-        } else if (bEvent & BUTTON_EVENT_2DOWN) {
-            ToneGeneration_SetFrequency(average + TONE_293);
-            ToneGeneration_ToneOn();
-        } else if (bEvent & BUTTON_EVENT_3DOWN) {
-            ToneGeneration_SetFrequency(average + TONE_440);
-            ToneGeneration_ToneOn();
-        } else if (bEvent & BUTTON_EVENT_4DOWN) {
-            ToneGeneration_SetFrequency(average + TONE_659);
-            ToneGeneration_ToneOn();
-        } else if (BUTTON_EVENT_1UP | BUTTON_EVENT_2UP | BUTTON_EVENT_3UP | BUTTON_EVENT_4UP) {
-            ToneGeneration_ToneOff();
-
+        switch (bEvent) {
+            case BUTTON_EVENT_1DOWN:
+                button_avg_flag = TRUE;
+                frequency = TONE_196;
+                ToneGeneration_SetFrequency(frequency);
+                ToneGeneration_ToneOn();
+                break;
+            case BUTTON_EVENT_2DOWN:
+                button_avg_flag = TRUE;
+                frequency = TONE_293;
+                ToneGeneration_SetFrequency(frequency);
+                ToneGeneration_ToneOn();
+                break;
+            case BUTTON_EVENT_3DOWN:
+                button_avg_flag = TRUE;
+                frequency = TONE_440;
+                ToneGeneration_SetFrequency(frequency);
+                ToneGeneration_ToneOn();
+                break;
+            case BUTTON_EVENT_4DOWN:
+                button_avg_flag = TRUE;
+                frequency = TONE_500;
+                ToneGeneration_SetFrequency(frequency);
+                ToneGeneration_ToneOn();
+                break;
+            case BUTTON_EVENT_1UP:
+            case BUTTON_EVENT_2UP:
+            case BUTTON_EVENT_3UP:
+            case BUTTON_EVENT_4UP:
+                ToneGeneration_ToneOff();
+                button_avg_flag = FALSE;
+                break;
         }
+
+        if (button_avg_flag) {
+            unsigned int new_pot_val = AD_ReadADPin(AD_A0);
+            sum = sum - move_average_readings[index] + new_pot_val; // subtract oldest reading and add new one
+            move_average_readings[index] = new_pot_val; // store the new reading
+            average = sum / sample_size;
+          
+            index = (index + 1) % sample_size;
+            if (abs(new_average - average) >= 5) {
+                new_average = average;
+                ToneGeneration_SetFrequency((average/5) + frequency);
+                ToneGeneration_ToneOn();
+            }
+        }
+        //        unsigned int new_pot_val = AD_ReadADPin(AD_A0);
+        //        if (pot_val != new_pot_val) {
+        //            pot_val = new_pot_val;
+        //            //printf("%d\n", pot_val);
+        //        }
+        //        sum = sum - move_average_readings[index] + new_pot_val; // subtract oldest reading and add new one
+        //        move_average_readings[index] = new_pot_val; // store the new reading
+        //
+        //        unsigned int average = sum / sample_size; // calculate the moving average
+        //        printf("%d\n", average);
+        //
+        //        index = (index + 1) % sample_size; // this will increment and wrap index
+        //        if (bEvent & BUTTON_EVENT_1DOWN) {
+        //            ToneGeneration_SetFrequency(average + TONE_196);
+        //            ToneGeneration_ToneOn();
+        //        } else if (bEvent & BUTTON_EVENT_2DOWN) {
+        //            ToneGeneration_SetFrequency(average + TONE_293);
+        //            ToneGeneration_ToneOn();
+        //        } else if (bEvent & BUTTON_EVENT_3DOWN) {
+        //            ToneGeneration_SetFrequency(average + TONE_440);
+        //            ToneGeneration_ToneOn();
+        //        } else if (bEvent & BUTTON_EVENT_4DOWN) {
+        //            ToneGeneration_SetFrequency(average + TONE_659);
+        //            ToneGeneration_ToneOn();
+        //        } else if (bEvent & (BUTTON_EVENT_1UP | BUTTON_EVENT_2UP | BUTTON_EVENT_3UP | BUTTON_EVENT_4UP)) {
+        //            ToneGeneration_ToneOff();
+        //
+        //        }
 
         //        ToneGeneration_SetFrequency(average); // This was used to test the speaker with pot, filter, and moving average
         //        ToneGeneration_ToneOn();
