652b49247fdc2b7ab4ecc760065dc3d7c90ee6be
Nadia Shabbar <nshabbar@ucsc.edu>
2024-01-18 21:06:44 -0800

    Updating progress. Made progress with mapping and getting the flex sensor to make noise.

---
+++ Lab1/Lab01.X/build/default/production/main.o.d
@@ -0,0 +1,12 @@
+build/default/production/main.o: main.c AD.h BOARD.h Buttons.h serial.h \
+ ToneGeneration.h
+
+AD.h:
+
+BOARD.h:
+
+Buttons.h:
+
+serial.h:
+
+ToneGeneration.h:
--- Lab1/Lab01.X/main.c
+++ Lab1/Lab01.X/main.c
@@ -13,10 +13,16 @@
 #include "Buttons.h"
 #include "serial.h"
 #include "ToneGeneration.h"
+#include <math.h>
 
 #define sample_size 150
-uint8_t button_avg_flag = FALSE;
+
 unsigned int frequency = DEFAULT_TONE;
+
+unsigned int flex_frequency(unsigned int AD_val){
+    unsigned int freq_output = 0.843*AD_val + 528;
+    return freq_output;
+}
 
 int main(int argc, char** argv) {
     BOARD_Init();
@@ -25,50 +31,16 @@ int main(int argc, char** argv) {
     AD_AddPins(AD_A1); // turning on pin for flex sensor
 
     BOARD_End();
-    unsigned int flex_val = 0;
+    
     unsigned int move_average_readings[sample_size] = {0}; // array to store samples of frequency
     unsigned int index = 0; // current index
     unsigned int sum = 0; // sum of all readings
     unsigned int new_average = 0;
-    unsigned int average =0;
+    unsigned int average = 0;
 
 
     while (1) {
-//        uint8_t bEvent = ButtonsCheckEvents();
-//        switch (bEvent) {
-//            case BUTTON_EVENT_1DOWN:
-//                button_avg_flag = TRUE;
-//                frequency = TONE_196;
-//                ToneGeneration_SetFrequency(frequency);
-//                ToneGeneration_ToneOn();
-//                break;
-//            case BUTTON_EVENT_2DOWN:
-//                button_avg_flag = TRUE;
-//                frequency = TONE_293;
-//                ToneGeneration_SetFrequency(frequency);
-//                ToneGeneration_ToneOn();
-//                break;
-//            case BUTTON_EVENT_3DOWN:
-//                button_avg_flag = TRUE;
-//                frequency = TONE_440;
-//                ToneGeneration_SetFrequency(frequency);
-//                ToneGeneration_ToneOn();
-//                break;
-//            case BUTTON_EVENT_4DOWN:
-//                button_avg_flag = TRUE;
-//                frequency = TONE_500;
-//                ToneGeneration_SetFrequency(frequency);
-//                ToneGeneration_ToneOn();
-//                break;
-//            case BUTTON_EVENT_1UP:
-//            case BUTTON_EVENT_2UP:
-//            case BUTTON_EVENT_3UP:
-//            case BUTTON_EVENT_4UP:
-//                ToneGeneration_ToneOff();
-//                button_avg_flag = FALSE;
-//                break;
-//        }
-//
+
 //        if (button_avg_flag) {
 //            unsigned int new_flex_val = AD_ReadADPin(AD_A0);
 //            sum = sum - move_average_readings[index] + new_flex_val; // subtract oldest reading and add new one
@@ -82,20 +54,24 @@ int main(int argc, char** argv) {
 //                ToneGeneration_ToneOn();
 //            }
 //        }
-                unsigned int new_flex_val = AD_ReadADPin(AD_A1); // Pin for Flex sensor
-                if (flex_val != new_flex_val) {
-                    flex_val = new_flex_val;
-//                    printf("%d\n", flex_val);
-                }
+                unsigned int flex_val = AD_ReadADPin(AD_A1);
+                unsigned int new_flex_val = flex_frequency(flex_val); // Pin for Flex sensor
+
+                
                 sum = sum - move_average_readings[index] + new_flex_val; // subtract oldest reading and add new one
                 move_average_readings[index] = new_flex_val; // store the new reading
         
-                unsigned int average = sum / sample_size; // calculate the moving average
-                printf("%d\n", average);
+                average = sum / sample_size; // calculate the moving average
+                
         
                 index = (index + 1) % sample_size; // this will increment and wrap index
         //        
-
+                if (abs(new_average - average) >= 5) {
+                    new_average = average;
+                    printf("%d\n", new_average);
+                    ToneGeneration_SetFrequency(((average-500)/2) + frequency);
+                    ToneGeneration_ToneOn();
+                }
         //        ToneGeneration_SetFrequency(average); // This was used to test the speaker with flex sensor, filter, and moving average
         //        ToneGeneration_ToneOn();
 
