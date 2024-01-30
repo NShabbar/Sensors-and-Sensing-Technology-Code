807b2fad37e7850fef15e0b521316c47789f921d
Nadia Shabbar <nshabbar@ucsc.edu>
2024-01-30 15:59:23 -0800

    Progress on Part 2.

---
+++ Lab2/PING.X/PING.c
@@ -0,0 +1,78 @@
+/* 
+ * File:   PING.c
+ * Author: Nadia
+ *
+ * Created on January 30, 2024, 3:44 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <PING.h>
+#include <BOARD.h>
+#include <serial.h>
+#include <timers.h>
+#include <ToneGeneration.h>
+
+
+static unsigned int time_of_flight;
+
+
+char PING_Init(void) {
+    // following block inits the timer
+    T4CON = 0;
+    T4CONbits.TCKPS = 0b110;
+    PR4 = 0xFFFF; // this is not the timer value wanted
+    T4CONbits.ON = 1;
+    IFS0bits.T4IF = 0;
+    IPC4bits.T4IP = 3;
+    IEC0bits.T4IE = 1;
+
+    // following block inits change notify
+    CNCONbits.ON = 1; // Change Notify On
+    CNENbits.CNEN14 = 1;
+    int temp = PORTD; // this is intentional to ensure a interrupt occur immediately upon enabling
+    IFS1bits.CNIF = 0; // clear interrupt flag
+    IPC6bits.CNIP = 1; //set priority
+    IPC6bits.CNIS = 3; // and sub priority
+    IEC1bits.CNIE = 1; // enable change notify
+    //Anything else that needs to occur goes here
+}
+
+void __ISR(_CHANGE_NOTICE_VECTOR) ChangeNotice_Handler(void) {
+    static char readPort = 0;
+    readPort = PORTD; // this read is required to make the interrupt work
+    IFS1bits.CNIF = 0;
+    //Anything else that needs to occur goes here
+}
+
+void __ISR(_TIMER_4_VECTOR) Timer4IntHandler(void) {
+    IFS0bits.T4IF = 0;
+    //Anything else that needs to occur goes here
+}
+
+
+/**
+ * @function    PING_GetDistance(void)
+ * @brief       Returns the calculated distance in mm using the sensor model determined
+ *              experimentally. 
+ *              No I/O should be done in this function
+ * @return      distance in mm
+ */
+unsigned int PING_GetDistance(void){
+    unsigned int distance = (unsigned int)((0.5 * 34000 * time_of_flight) / 1000000);
+}
+
+/**
+ * @function    PING_GetTimeofFlight(void)
+ * @brief       Returns the raw microsecond duration of the echo from the sensor.
+ *              NO I/O should be done in this function.
+ * @return      time of flight in uSec
+ */
+unsigned int PING_GetTimeofFlight(void);
+
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
