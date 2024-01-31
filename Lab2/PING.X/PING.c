a6d95ec91f00d0e4299c9f622ac5f23fd6983ef8
Nadia Shabbar <nshabbar@ucsc.edu>
2024-01-30 19:31:05 -0800

    Got the distance working with the interrupts.

--- Lab2/PING.X/PING.c
+++ Lab2/PING.X/PING.c
@@ -15,25 +15,24 @@
 #include <ToneGeneration.h>
 
 
-
+#define trigger LATDbits.LATD3
+#define echo PORTDbits.RD5
 
 static volatile unsigned int time_of_flight;
 static volatile unsigned int start_time;
-static volatile unsigned int echo = 0;
-static volatile unsigned int trigger = 0;
-int current_state = 0;
+int trigger_count = 0;
+//int current_state = 0;
 
-typedef enum{
-    start = 0,
-    waiting = 1   
-} ping_states;
-
+//typedef enum{
+//    start = 0,
+//    waiting = 1   
+//} ping_states;
 
 char PING_Init(void) {
     // following block inits the timer
     T4CON = 0;
-    T4CONbits.TCKPS = 0b110;
-    PR4 = 3750; // this is not the timer value wanted (changed o 3750)
+    T4CONbits.TCKPS = 0b110; // 6 in binary
+    PR4 = 6; // this is not the timer value wanted (changed to 3750)
     T4CONbits.ON = 1;
     IFS0bits.T4IF = 0;
     IPC4bits.T4IP = 3;
@@ -48,6 +47,13 @@ char PING_Init(void) {
     IPC6bits.CNIS = 3; // and sub priority
     IEC1bits.CNIE = 1; // enable change notify
     //Anything else that needs to occur goes here
+
+    //TRIS sets data direction flow.
+    TRISDbits.TRISD5 = 1; // echo, input needs to be high for input
+    TRISDbits.TRISD3 = 0; // trigger, output needs to be low for output
+    //LAT is effectively the same as writing to the register.
+    LATDbits.LATD3 = 1; // for data corresponding to data
+
     return SUCCESS;
 }
 
@@ -56,35 +62,46 @@ void __ISR(_CHANGE_NOTICE_VECTOR) ChangeNotice_Handler(void) {
     readPort = PORTD; // this read is required to make the interrupt work
     IFS1bits.CNIF = 0;
     //Anything else that needs to occur goes here
-    echo = PORTDbits.RD4;
-    printf("Pinging");
-    switch(current_state){
-        case start:
-            if (echo){
-                start_time = TIMERS_GetMicroSeconds();
-                current_state = waiting;
-            }
-            break;
-        case waiting:
-            if (!echo){
-                unsigned int end_time = TIMERS_GetMicroSeconds();
-                time_of_flight = end_time - start_time;
-                current_state = start;
-            }
-            break;
+
+    if (echo) {
+        start_time = TIMERS_GetMicroSeconds();
     }
+
+    if (!echo) {
+        unsigned int end_time = TIMERS_GetMicroSeconds();
+        time_of_flight = end_time - start_time;
+        printf("Time of Flight in cm: %d\n", time_of_flight/58);
+        printf("Distance: %d\n\n", PING_GetDistance());
+    }
+
+    //    switch(current_state){
+    //        case start:
+    //            if (echo){
+    //                start_time = TIMERS_GetMicroSeconds();
+    //                current_state = waiting;
+    //            }
+    //            break;
+    //        case waiting:
+    //            if (!echo){
+    //                unsigned int end_time = TIMERS_GetMicroSeconds();
+    //                time_of_flight = end_time - start_time;
+    //                current_state = start;
+    //            }
+    //            break;
+    //    }
 }
 
 void __ISR(_TIMER_4_VECTOR) Timer4IntHandler(void) {
     IFS0bits.T4IF = 0;
-            
-    trigger = 1;
-    while (TMR4 < 400);
-    trigger = 0;
-    
     //Anything else that needs to occur goes here
+    trigger_count++;
+    if (trigger_count > 6000) {
+        trigger_count = 0;
+        trigger = 1;
+    } else {
+        trigger = 0;
+    }
 }
-
 
 /**
  * @function    PING_GetDistance(void)
@@ -93,10 +110,10 @@ void __ISR(_TIMER_4_VECTOR) Timer4IntHandler(void) {
  *              No I/O should be done in this function
  * @return      distance in mm
  */
-unsigned int PING_GetDistance(void){
+unsigned int PING_GetDistance(void) {
     // 34000 comes from the speed of sound. 340 meters/seconds * 1000 mm/ meter
     // 1000000 comes from 1 mil microseconds in a second.
-    unsigned int distance = (unsigned int)((0.5 * 34000 * time_of_flight) / 1000000);
+    unsigned int distance = (unsigned int) ((0.5 * 34000 * time_of_flight) / 1000000);
     return distance;
 }
 
@@ -106,23 +123,16 @@ unsigned int PING_GetDistance(void){
  *              NO I/O should be done in this function.
  * @return      time of flight in uSec
  */
-unsigned int PING_GetTimeofFlight(void){
+unsigned int PING_GetTimeofFlight(void) {
     return time_of_flight;
 }
-
 
 int main(int argc, char** argv) {
     BOARD_Init();
     TIMERS_Init();
     ToneGeneration_Init();
-    PWM_Init();
-    
-    PWM_AddPins(PWM_PORTY04);
-    while(1){
-        if (trigger){
-            printf("trigger\n");
-        }
-    }
+    PING_Init();
+    while (1);
     return (EXIT_SUCCESS);
 }
 
