afc81ca360c2aac467eaf8190ae8f61706d9a8b1
Nadia Shabbar <nshabbar@ucsc.edu>
2024-01-31 12:26:06 -0800

    Finished part 2.

--- Lab2/PING.X/PING.c
+++ Lab2/PING.X/PING.c
@@ -15,18 +15,34 @@
 #include <ToneGeneration.h>
 
 
-#define trigger LATDbits.LATD3
-#define echo PORTDbits.RD5
+#define trigger LATDbits.LATD3 // Pin 9
+#define echo PORTDbits.RD5 // Pin 34
+#define sample_size 15
 
 static volatile unsigned int time_of_flight;
 static volatile unsigned int start_time;
 int trigger_count = 0;
-//int current_state = 0;
 
-//typedef enum{
-//    start = 0,
-//    waiting = 1   
-//} ping_states;
+unsigned int frequency = DEFAULT_TONE;
+unsigned int move_average_readings[sample_size] = {0}; // array to store samples of frequency
+unsigned int index = 0; // current index
+unsigned int sum = 0; // sum of all readings
+unsigned int new_average = 0;
+unsigned int average = 0;
+unsigned int new_reading = 0;
+
+int map(int moving_avg, int ping_min, int ping_max, int out_min, int out_max) {
+    int scaled_value = (moving_avg - ping_min) * (out_max - out_min) / (ping_max - ping_min) + out_min;
+
+    // makes sure the result does not exceed the specified range
+    if (scaled_value > out_max) {
+        scaled_value = out_max;
+    }// makes sure the result does not go below the specified range
+    else if (scaled_value < out_min) {
+        scaled_value = out_min;
+    }
+    return scaled_value;
+}
 
 char PING_Init(void) {
     // following block inits the timer
@@ -70,34 +86,20 @@ void __ISR(_CHANGE_NOTICE_VECTOR) ChangeNotice_Handler(void) {
     if (!echo) {
         unsigned int end_time = TIMERS_GetMicroSeconds();
         time_of_flight = end_time - start_time;
-        printf("Time of Flight in cm: %d\n", time_of_flight/58);
+        
+        printf("Output of Moving Average: %d\n", average);
+        printf("Time of Flight in cm: %d\n", time_of_flight / 58);
         printf("Distance: %d\n\n", PING_GetDistance());
     }
-
-    //    switch(current_state){
-    //        case start:
-    //            if (echo){
-    //                start_time = TIMERS_GetMicroSeconds();
-    //                current_state = waiting;
-    //            }
-    //            break;
-    //        case waiting:
-    //            if (!echo){
-    //                unsigned int end_time = TIMERS_GetMicroSeconds();
-    //                time_of_flight = end_time - start_time;
-    //                current_state = start;
-    //            }
-    //            break;
-    //    }
 }
 
 void __ISR(_TIMER_4_VECTOR) Timer4IntHandler(void) {
     IFS0bits.T4IF = 0;
     //Anything else that needs to occur goes here
     trigger_count++;
-    if (trigger_count > 6000) {
+    if (trigger_count > 6000) { // 6000 for 60ms, reset after.
         trigger_count = 0;
-        trigger = 1;
+        trigger = 1; // set trigger pin high
     } else {
         trigger = 0;
     }
@@ -132,7 +134,20 @@ int main(int argc, char** argv) {
     TIMERS_Init();
     ToneGeneration_Init();
     PING_Init();
-    while (1);
+
+    while (1) {
+        new_reading = PING_GetDistance();
+
+        sum = sum - move_average_readings[index] + new_reading; // subtract oldest reading and add new one
+        move_average_readings[index] = new_reading; // store the new reading
+
+        average = sum / sample_size; // calculate the moving average
+
+        index = (index + 1) % sample_size; // this will increment and wrap index
+
+        int hertz = map(average, 5, 350, 100, 4000);
+        ToneGeneration_SetFrequency(hertz);
+    }
     return (EXIT_SUCCESS);
 }
 
---
+++ Lab2/PING.X/build/default/production/AD.o.d
@@ -0,0 +1,8 @@
+build/default/production/AD.o: AD.c ../../Common/BOARD.h \
+ ../../Common/AD.h ../../Common/serial.h
+
+../../Common/BOARD.h:
+
+../../Common/AD.h:
+
+../../Common/serial.h:
