07c4d094d5527d463627a442940fe856451388ec
Nadia Shabbar <nshabbar@ucsc.edu>
2024-01-30 17:12:19 -0800

    Adding what progress I've made to git.

---
+++ Lab2/PING.X/build/default/production/PING.o.d
@@ -0,0 +1,17 @@
+build/default/production/PING.o: PING.c ../../Common/PING.h \
+ ../../Common/BOARD.h ../../Common/BOARD.h ../../Common/serial.h \
+ ../../Common/timers.h ../../Common/pwm.h ../../Common/ToneGeneration.h
+
+../../Common/PING.h:
+
+../../Common/BOARD.h:
+
+../../Common/BOARD.h:
+
+../../Common/serial.h:
+
+../../Common/timers.h:
+
+../../Common/pwm.h:
+
+../../Common/ToneGeneration.h:
---
+++ Lab2/PING.X/build/default/production/timers.o.d
@@ -0,0 +1,5 @@
+build/default/production/timers.o: timers.c ../../Common/BOARD.h timers.h
+
+../../Common/BOARD.h:
+
+timers.h:
