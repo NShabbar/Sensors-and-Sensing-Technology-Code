31e5671901f4e48a2ea25a7b755f054ef247d67f
Nadia Shabbar <nshabbar@ucsc.edu>
2024-01-31 21:50:15 -0800

    Finished part 3, maybe?

--- Lab2/CAPTOUCH.X/CAPTOUCH.c
+++ Lab2/CAPTOUCH.X/CAPTOUCH.c
@@ -15,16 +15,17 @@
 #define TOUCH_THRESHOLD 1000
 #define CapTouch PORTDbits.RD11 // Pin 35
 
-static volatile unsigned int touch_period;
-static volatile unsigned int prev_touch_period;
-static volatile unsigned int curr_timer_val;
+int touch_period;
+int prev_touch_period;
+int curr_timer_val;
 
-unsigned int move_average_readings[TOUCH_THRESHOLD] = {0};
-unsigned int i = 0; // current index
-unsigned int sum = 0; // sum of all readings
-unsigned int new_average = 0;
-unsigned int average = 0;
-unsigned int new_reading = 0;
+int move_average_readings[TOUCH_THRESHOLD] = {0};
+int i = 0; // current index
+int sum = 0; // sum of all readings
+int new_average = 0;
+int average = 0;
+int new_reading = 0;
+int is_touched = 0;
 
 char CAPTOUCH_Init(void) {
     // following block inits the timer
@@ -52,12 +53,11 @@ void __ISR(_INPUT_CAPTURE_4_VECTOR) InputCapture_Handler(void) {
     IFS0bits.IC4IF = 0;
     // IC4BUF contains the timer value when the rising edge occurred.
     curr_timer_val = IC4BUF;
-    int is_touched = CAPTOUCH_IsTouched();
-    if (is_touched) {
-        touch_period = curr_timer_val - prev_touch_period;
-        prev_touch_period = curr_timer_val;
-        printf("Time: %d\n", touch_period);
+    touch_period = curr_timer_val - prev_touch_period;
+    if (touch_period < 0) {
+        touch_period += 65536;
     }
+    prev_touch_period = curr_timer_val;
 }
 
 /**
@@ -68,18 +68,7 @@ void __ISR(_INPUT_CAPTURE_4_VECTOR) InputCapture_Handler(void) {
  * @return      TRUE or FALSE
  */
 char CAPTOUCH_IsTouched(void) {
-    new_reading = CapTouch;
-
-    sum = sum - move_average_readings[i] + new_reading; // subtract oldest reading and add new one
-    move_average_readings[i] = new_reading; // store the new reading
-
-    average = sum / TOUCH_THRESHOLD; // calculate the moving average
-
-    i = (i + 1) % TOUCH_THRESHOLD; // this will increment and wrap index
-    if (abs(new_average - average) >= 2000) {
-        new_average = average;
-    }
-    return (new_average > TOUCH_THRESHOLD) ? TRUE : FALSE;
+    return is_touched;
 }
 
 /*
@@ -90,7 +79,33 @@ int main(int argc, char** argv) {
     SERIAL_Init();
     TIMERS_Init();
     CAPTOUCH_Init();
-    while (1);
+
+    while (1) {
+        new_reading = touch_period;
+
+        sum = sum - move_average_readings[i] + new_reading; // subtract oldest reading and add new one
+        move_average_readings[i] = new_reading; // store the new reading
+
+        average = sum / TOUCH_THRESHOLD; // calculate the moving average
+
+        i = (i + 1) % TOUCH_THRESHOLD; // this will increment and wrap index
+        //        if (abs(new_average - average) >= 2000) {
+        //            new_average = average;
+        //        }
+
+        if (is_touched) {
+            if (average < 6000) {
+                printf("I am not being touched.\n");
+                is_touched = 0;
+            }
+
+        } else {
+            if (average > 10000) {
+                printf("I am being touched.\n");
+                is_touched = 1;
+            }
+        }
+    }
     return (EXIT_SUCCESS);
 }
 
